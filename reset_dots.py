2c2640cea57544e0bab7ca1c4f79f811e286cbf9
Marshall Baughman <95940057+mbaughman7@users.noreply.github.com>
2023-01-20 15:24:38 -0600

    Update reset_dots.py

--- reset_dots.py
+++ reset_dots.py
@@ -5,5 +5,5 @@ dots = dotstar.DotStar(board.D6, board.D5, 3, brightness=0.2)
 
 n_dots = len(dots)
 
-  for dot in range(n_dots):
-      dots[dot] = (0, 0, 0)
+for dot in range(n_dots):
+    dots[dot] = (0, 0, 0)
